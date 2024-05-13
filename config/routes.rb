0dccf9b379d7a1387a6da1d27ab653f66ed587b1
vinay sharma <vinayrordeveloper@gmail.com>
2024-05-13 16:18:39 +0530

    added changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       sessions: 'users/sessions'
     }
- 
+
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+
   resources :attendances, only: []
   resources :events do
     resources :attendances, only: [:create, :destroy], controller: 'attendances'
